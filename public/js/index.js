ff04405291ec1e58fc9017b353f35af9c4976d6e
Rad Cirskis <nad2000@gmail.com>
2018-02-10 23:54:37 +1300

    improve usability ...scroll if the user is close to the bottom.

--- public/js/index.js
+++ public/js/index.js
@@ -8,15 +8,25 @@ socket.on("disconnect", function() {
   console.log("Disconnected from the server");
 });
 
-function fTime(msg) {
-  return moment(msg.createdAt).format("h:mm a");
-}
-
 function appendMsg(msg) {
-  msg.formattedTime = fTime(msg);
+  msg.formattedTime = moment(msg.createdAt).format("h:mm a");
   var template = jQuery("#message-template").html();
   var html = Mustache.render(template, msg);
   jQuery("#messages").append(html);
+  scrollToBottom();
+}
+
+function scrollToBottom () {
+  var messages = jQuery("#messages");
+  var newMessage = messages.children("li:last-child");
+  var clientHight = messages.prop("clientHeight"); // jQuery cross-broweser prop...
+  var scrollTop = messages.prop("scrollTop");
+  var scrollHeight = messages.prop("scrollHeight");
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
 }
 
 socket.on("newMessage", appendMsg);
